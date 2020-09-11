718b2d5e13eea6555bf92e9d91fc4dc4ec537aed
Son Do Hong <sondh0127@gmail.com>
2020-09-12 01:23:21 +0700

    upgrade retail to fix purge css problem

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,15 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const { resolveConfig } = require('@retail-ui/core')
 
 module.exports = resolveConfig({
-  purge: ['{src}/**/*.{js,jsx,ts,tsx}'],
-  experimental: 'all',
+  purge: ['src/**/*.{js,jsx,ts,tsx}'],
+  experimental: {
+    uniformColorPalette: true,
+    extendedSpacingScale: true,
+    defaultLineHeights: true,
+    extendedFontSizeScale: true,
+    applyComplexClasses: true,
+    standardFontWeights: true,
+  },
   theme: {
     extend: {
       fontFamily: {
