5a4c85f2ff0ca6cac11d182130fede8c743af5cf
Son Hong Do <sondh0127@gmail.com>
2022-04-01 09:58:22 +0700

    rebuild

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -3,7 +3,9 @@
 declare global {
   const useCallback: typeof import('react')['useCallback']
   const useContext: typeof import('react')['useContext']
+  const useDebugValue: typeof import('react')['useDebugValue']
   const useEffect: typeof import('react')['useEffect']
+  const useImperativeHandle: typeof import('react')['useImperativeHandle']
   const useMemo: typeof import('react')['useMemo']
   const useReducer: typeof import('react')['useReducer']
   const useRef: typeof import('react')['useRef']
